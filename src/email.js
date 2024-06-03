7f2784ac7faaea3d08bb34d4a39e5e684701f621
topmaster29 <smartdevtop1989@gmail.com>
2024-06-03 11:14:19 -0400

    fix mailtransporter

--- src/email.js
+++ src/email.js
@@ -3,27 +3,29 @@ const nodemailer = require("nodemailer");
 const godaddyEmail = process.env.GODADDY_EMAIL;
 const godaddyPassword = process.env.GODADDY_EMAIL_PASSWORD;
 
-const mailTransport = nodemailer.createTransport({
-	host: "smtpout.secureserver.net",
-	secure: true,
-	secureConnection: false, // TLS requires secureConnection to be false
-	tls: {
-		ciphers: "SSLv3",
-	},
-	requireTLS: true,
-	port: 465,
-	debug: true,
-	auth: {
-		user: godaddyEmail,
-		pass: godaddyPassword,
-	},
-});
+
 
 /**
  * @description send email
  */
 const sendMail = async (mailOption) => {
   try {
+    const mailTransport = nodemailer.createTransport({
+      host: "smtpout.secureserver.net",
+      secure: true,
+      secureConnection: false, // TLS requires secureConnection to be false
+      tls: {
+        ciphers: "SSLv3",
+      },
+      requireTLS: true,
+      port: 465,
+      debug: true,
+      auth: {
+        user: godaddyEmail,
+        pass: godaddyPassword,
+      },
+    });
+    
     console.log("sending email...")
 		await mailTransport.sendMail(mailOption);
     console.log("email sent")
